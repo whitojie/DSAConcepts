c2ed3a18b2784f68de181b785384eb64c59d24ce
whitojie <whit.ojie@gmail.com>
2024-11-08 22:18:28 -0500

    DSA CONCEPTS

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+
+using namespace std;
+int main() {
+    /* BINARY SEARCH TREE
+
+    BST tree;
+
+    tree.insert(new Node(5));
+    tree.insert(new Node(1));
+    tree.insert(new Node(9));
+    tree.insert(new Node(2));
+    tree.insert(new Node(7));
+    tree.insert(new Node(3));
+    tree.insert(new Node(6));
+    tree.insert(new Node(4));
+    tree.insert(new Node(8));
+
+    tree.display();
+    tree.remove(9);
+    cout << tree.search(10) << endl; */
+
+
+
+
+    /* STACKS AND QUEUES
+     *
+     * Stack is a data structure with last in first out structure (cannot take out any other
+     * element until you take the first one out on top
+     *
+     * Queues are a type of data structure the operates a first in first out
+
+    stack<string> stk;
+
+    // add each item to the stack
+    stk.push("Eggs");
+    stk.push("Bacon");
+    stk.push("Grits");
+    stk.push("Sausage");
+    stk.push("Cereal");
+
+    //while stack isn't empty...
+    while (!stk.empty()) {
+        //show each top
+        cout << stk.top() << endl;
+        //pops the current element to show the next one on top
+        stk.pop();
+    }
+
+    queue<int> queue;
+
+    queue.push(23);
+    queue.push(34);
+    queue.push(98);
+    queue.push(4);
+    queue.push(18);
+
+    cout << "Front of the queue is " << queue.front() << endl;
+    cout << "Back of the queue is " << queue.back() << endl;
+    cout << "Size of the queue is " << queue.size() << endl;
+    while(!queue.empty()){
+        // check the front of the queue
+        cout << queue.front() << endl;
+        //pop the front element and recheck the front in the loop
+        queue.pop();
+    }
+    */
+
+
+}
